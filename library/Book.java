3422ad7d2704a12288ca7463c683a43580632047
Neuboi <sadjei1@lsu.edu>
2019-03-20 14:52:32 -0500

    Day One 3/20
    Started the project. Book and LibraryBook completed for the most part

---
+++ library/Book.java
@@ -0,0 +1,39 @@
+package library;
+
+public class Book
+{
+   protected String author;
+   protected String title;
+   protected String isbn;
+   
+   public Book(){
+     author = "";
+     title = "";
+     isbn = "";
+   }
+   
+   public Book(String a, String t, String i){
+     author = a;
+     title = t;
+     isbn = i;
+   }
+   
+   public void getAuthor(){
+   }
+   public void getTitle(){
+   }
+   public void getISBN(){
+   }
+   
+   public void setAuthor(){
+   }
+   public void setTitle(){
+   }
+   public void setISBN(){
+   }
+   
+   public String toString(){
+    return "Author: "+author+"\nTitle: "+title+"\nISBN: "+isbn+"\n"; 
+   }
+   
+}
